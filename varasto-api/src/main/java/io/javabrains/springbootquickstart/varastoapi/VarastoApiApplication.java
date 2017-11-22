92049fe01611cfe4720dcb81298f6fa64408e8d7
Henrik Hartiala <hartialh@student.tut.fi>
2017-11-22 12:51:57 +0200

    Uus rakenne

---
+++ varasto-api/src/main/java/io/javabrains/springbootquickstart/varastoapi/VarastoApiApplication.java
@@ -0,0 +1,12 @@
+package io.javabrains.springbootquickstart.varastoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarastoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VarastoApiApplication.class, args);
+	}
+}
