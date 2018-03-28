14fe657094d31ca2c0eff773a0e1343d03f69efa
Henrik Hartiala <hartialh@student.tut.fi>
2018-03-28 17:40:22 +0300

    Bugi korjattu, monen itemin apikutsu oli "/api/items/" korjaus: "/api/items"

--- varasto-api/src/main/java/net/pirkat/varastoapi/controller/ItemController.java
+++ varasto-api/src/main/java/net/pirkat/varastoapi/controller/ItemController.java
@@ -15,34 +15,40 @@ public class ItemController {
     private ItemService itemService;
 
     //Get-request for all items in a category
-    @RequestMapping("/categories/{id}/items")
+    @RequestMapping("/api/categories/{id}/items")
     public List<Item> getAllItems(@PathVariable Long id){
         return itemService.getAllItems(id);
     }
 
     //Get-request for a particular item in a category
-    @RequestMapping("/categories/{categoryId}/items/{id}")
+    @RequestMapping("/api/categories/{categoryId}/items/{id}")
     public Item getItem(@PathVariable Long id) {
         return itemService.getItem(id);
     }
 
     //Post-request for adding new item to a category
-    @RequestMapping(method=RequestMethod.POST, value="/categories/{categoryId}/items")
+    @RequestMapping(method=RequestMethod.POST, value="/api/categories/{categoryId}/items")
     public Item addItem(@RequestBody Item item, @PathVariable Long categoryId){
         item.setCategory(new Category(categoryId, "", ""));
         return itemService.addItem(item);
     }
 
     //Update item and set a category for it
-    @RequestMapping(method=RequestMethod.PUT, value="/categories/{categoryId}/items/{id}")
+    @RequestMapping(method=RequestMethod.PUT, value="/api/categories/{categoryId}/items/{id}")
     public Item updateItem(@RequestBody Item item, @PathVariable Long id, @PathVariable Long categoryId){
         item.setId(id);
         item.setCategory(new Category(categoryId,"", ""));
         return itemService.updateItem(item);
     }
+
+    //Update multiple items (set borrowed-flag to true)
+    @RequestMapping(method=RequestMethod.PUT, value="/api/items")
+    public List<Item> updateItems(@RequestBody List<Item> items) {
+        return itemService.updateItems(items);
+    }
     
     //Delete item
-    @RequestMapping(method=RequestMethod.DELETE, value="/categories/{categoryId}/items/{id}")
+    @RequestMapping(method=RequestMethod.DELETE, value="/api/categories/{categoryId}/items/{id}")
     public void deleteItem(@PathVariable Long id){
         itemService.deleteItem(id);
     }
